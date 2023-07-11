3df382e27e24841b649f4b4f1653bbe00f4e8894
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-07-10 18:13:18 -0600

    extract components, crudely import reply icon'

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,5 +1,6 @@
 import iconMinus from "/assets/images/icon-minus.svg";
 import iconPlus from "/assets/images/icon-plus.svg";
+import iconReply from "/assets/images/icon-reply.svg";
 
 import { CommentProps } from "../types/types";
 
@@ -16,21 +17,50 @@ export function CommentCard({ comment }: CommentCardProps) {
   return (
     <div className="flex justify-around min-w-[50%] border-2 border-red-200">
       <UpvoteDownvoteButton />
-      <div>
-        {/* div here for header type stuff contains user + actions */}
-        {/* user stuff */}
-        <div>
-          <img src={webp} className="max-h-2rem" alt="profile pic" />
-          <p className="pl-2">{username}</p>
-          <p className="pl-2">{createdAt}</p>
-        </div>
-        {/* TODO action buttons */}
+      <ContentWrapper
+        avatar={webp}
+        username={username}
+        createdAt={createdAt}
+        content={content}
+      />
+    </div>
+  );
+}
 
-        {/* text content */}
-        <div>
-          <p>{content}</p>
-        </div>
+function ContentWrapper({ avatar, username, createdAt, content }) {
+  const user = {
+    avatar,
+    username,
+    createdAt,
+  };
+
+  return (
+    <div>
+      <CardHeader user={user} />
+      <CommentContent content={content} />
+    </div>
+  );
+}
+
+function CardHeader({ user }) {
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="flex flex-row basic-1/2">
+        <img src={user.avatar} className="max-h-2rem" alt="profile pic" />
+        <p className="pl-2">{user.username}</p>
+        <p className="pl-2">{user.createdAt}</p>
       </div>
+      <div className="flex basis-1/2">
+        <img src={iconReply} className="max-h-2rem" alt="reply button" />
+      </div>
+    </div>
+  );
+}
+
+function CommentContent({ content }) {
+  return (
+    <div>
+      <p>{content}</p>
     </div>
   );
 }
@@ -38,7 +68,7 @@ export function CommentCard({ comment }: CommentCardProps) {
 function UpvoteDownvoteButton() {
   return (
     <div className="min-w-[3rem] p-2">
-      <div className="flex flex-col items-center  p-1 rounded-md bg-slate-200">
+      <div className="flex flex-col items-center p-1 rounded-md bg-slate-200">
         <img src={iconPlus} className="w-4 mb-4" alt="upvote" />
         <p className="mb-4">77</p>
         <img src={iconMinus} className="mb-4 w-4" alt="downvote" />
